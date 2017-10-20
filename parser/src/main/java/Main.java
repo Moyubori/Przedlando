1009fb29822654058609e278cb3611a38db124a9
Tymoteusz Buczkowski <moyubori@gmail.com>
2017-10-20 14:35:17 +0200

    Added scraping product URLs from catalog pages

--- parser/src/main/java/Main.java
+++ parser/src/main/java/Main.java
@@ -3,8 +3,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Scraper scraper = new Scraper();
-        scraper.scrap("https://www.zalando.pl/converse-sukienka-letnia-white-co421c000-a11.html");
+        Scraper scraper = new Scraper("https://www.zalando.pl", "/okazje/");
     }
 
 }
--- parser/src/main/java/Scraper.java
+++ parser/src/main/java/Scraper.java
@@ -1,6 +1,8 @@
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
@@ -12,12 +14,74 @@ import java.util.List;
 
 public class Scraper {
 
+    private static final int RETRY_LIMIT = 10;
+    private static final int PAGE_LIMIT = 1;
+
     private List<ScrapedProduct> scrapedProducts = new ArrayList<ScrapedProduct>();
 
-    public Scraper() {
+    public Scraper(String baseUrl, String catalogUrl) {
+        System.out.println("Scraping " + baseUrl + catalogUrl + ".");
+        int pages = 0;
+        boolean scrapingSuccessfull = false;
+        int retries = 0;
+        while(!scrapingSuccessfull) {
+            if(retries > RETRY_LIMIT) {
+                throw new NullPointerException("Can't scrape page " + baseUrl + catalogUrl + ". Retry limit exceeded.");
+            }
+            try {
+                String pageContents = getPage(baseUrl + catalogUrl);
+                Document parsedPage = Jsoup.parse(pageContents);
+                Elements elements = parsedPage.getElementsByClass("z-nvg-cognac_label-2W3Y8");
+                Element element = elements.first();
+                String str = element.text();
+                pages = Integer.parseInt(str.replaceFirst(".+[z ]",""));
+                System.out.println("Found " + pages + " pages.");
+                scrapingSuccessfull = true;
+            } catch (NullPointerException e) {
+                System.out.println("Couldn't scrap " + baseUrl + catalogUrl + ". Trying again...");
+                retries++;
+                scrapingSuccessfull = false;
+            }
+        }
+        if(PAGE_LIMIT < pages) {
+            pages = PAGE_LIMIT;
+        }
+        for(int i = 0; i < pages; i++) {
+            List<String> productUrls = scrapProductUrls(baseUrl, catalogUrl + "?p=" + (i+1));
+        }
     }
 
-    public void scrap(String url) {
+    private List<String> scrapProductUrls(String baseUrl, String catalogUrl) {
+        System.out.println("Scraping product URLs from " + baseUrl + catalogUrl + ".");
+        List<String> productUrls = new ArrayList<String>();
+        boolean scrapingSuccessfull = false;
+        int retries = 0;
+        while(!scrapingSuccessfull) {
+            if(retries > RETRY_LIMIT) {
+                throw new NullPointerException("Can't scrape page " + baseUrl + catalogUrl + ". Retry limit exceeded.");
+            }
+            try {
+                String pageContents = getPage(baseUrl + catalogUrl);
+                Document parsedPage = Jsoup.parse(pageContents);
+                Elements elements = parsedPage.getElementsByClass("z-nvg-cognac_imageLink-OPGGa");
+                if(elements.size() == 0) {
+                    throw new NullPointerException();
+                }
+                System.out.println("Found " + elements.size() + " products on page " + baseUrl + catalogUrl + ".");
+                for (Element element: elements) {
+                    productUrls.add(element.attr("href"));
+                }
+                scrapingSuccessfull = true;
+            } catch (NullPointerException e) {
+                System.out.println("Couldn't scrap " + baseUrl + catalogUrl + ". Trying again...");
+                retries++;
+                scrapingSuccessfull = false;
+            }
+        }
+        return productUrls;
+    }
+
+    private void scrapProduct(String url) {
         String rawPage = getPage(url);
         Document parsedPage = Jsoup.parse(rawPage);
         String productData = parsedPage.getElementById("z-vegas-pdp-props").data();
